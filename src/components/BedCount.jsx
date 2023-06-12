0accf6771f328cd7e813a22f8933d4a0a9556e42
Roho7 <rohosen2@gmail.com>
2023-06-12 08:26:50 +0530

    BedHover, Calendar removed

--- src/components/BedCount.jsx
+++ src/components/BedCount.jsx
@@ -1,11 +1,5 @@
 import React from "react";
 
-const handleBedHover = (e) => {
-  const bedHover = document.querySelector(".bed-hover");
-  bedHover.style.display = "block";
-  console.log("bedhover");
-};
-
 function BedCount(props) {
   const beds = props.data;
   const patient = props.patient.data;
@@ -14,25 +8,23 @@ function BedCount(props) {
     <div className="container col-span-3">
       <h1 className="text-3xl  font-bold mb-4">Vacancies</h1>
       <div className="flex flex-col gap-2">
-        <h2 className="font-bold text-sky-700">First Floor</h2>
+        {/* <h2 className="font-bold text-sky-700 ">First Floor</h2> */}
         <div className=" flex gap-4 flex-wrap">
           {beds.map((bed) => {
-            // console.log("name", patient.data.Name);
             return (
-              <div key={bed.id} className="relative">
-                <div className="bed-hover">
-                  {patient
-                    .filter((item) => item.Bed === bed.Name)
-                    .map((e) => e.Name)}
-                </div>
+              <div key={bed.id}>
                 <div
-                  onClick={handleBedHover}
                   className={
                     patient.some((item) => item.Bed === bed.Name)
-                      ? "bed occupied"
+                      ? "bed occupied group"
                       : "bed"
                   }
                 >
+                  <div className="bed-hover group-hover:scale-100">
+                    {patient
+                      .filter((item) => item.Bed === bed.Name)
+                      .map((e) => e.Name)}
+                  </div>
                   {bed.Name}
                 </div>
               </div>
--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,25 +1,10 @@
 import React from "react";
-import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
-import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import dayjs from "dayjs";
-import { DateCalendar } from "@mui/x-date-pickers/DateCalendar";
 
 function Calendar() {
   const today = new Date();
   return (
     <div className="container col-span-2 flex flex-col justify-between">
       <h1 className="text-3xl text-sky-900 font-bold mb-4">Calendar</h1>
-      <LocalizationProvider dateAdapter={AdapterDayjs}>
-        <DateCalendar
-          defaultValue={dayjs(today)}
-          sx={{
-            padding: "0px",
-            margin: "0px",
-            width: "100%",
-            height: "100%",
-          }}
-        />
-      </LocalizationProvider>
     </div>
   );
 }
